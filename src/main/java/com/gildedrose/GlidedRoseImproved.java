12503a98e4608adf0bf3c72a0df1ed72c904dd91
diavbolo <dmunoz@Diegos-MacBook-Pro.local>
2020-10-23 14:49:00 +0200

    Fix identation

--- src/main/java/com/gildedrose/GlidedRoseImproved.java
+++ src/main/java/com/gildedrose/GlidedRoseImproved.java
@@ -1,158 +1,162 @@
 package com.gildedrose;
 
 /**
-* The GlidedRoseImproved class, defined as a child of 
-* GildedRose, implements the handling of diverse items
-* regarding the expiration and the quality.
-*
-* @author  diavbolo
-* @version 1.0
-*/
+ * The GlidedRoseImproved class, defined as a child of GildedRose, implements
+ * the handling of diverse items regarding the expiration and the quality.
+ *
+ * @author diavbolo
+ * @version 1.0
+ */
 public class GlidedRoseImproved extends GildedRose {
-     int position = 0;
-    static final int MAXQUALITY = 50;
+	int position = 0;
+	static final int MAXQUALITY = 50;
 
-    /**
-     * Constructor that initialises using the parent class.
-     * @param Item[] List of items
-     */
-     public GlidedRoseImproved(Item[] items) {
-          super(items);
-     }
+	/**
+	 * Constructor that initialises using the parent class.
+	 * 
+	 * @param Item[] List of items
+	 */
+	public GlidedRoseImproved(Item[] items) {
+		super(items);
+	}
 
-    /**
-     * Getter that retrieves sellIn value.
-     */     
-     public int getItemSellIn() {
-          return items[position].sellIn;
-     }
+	/**
+	 * Getter that retrieves sellIn value.
+	 */
+	public int getItemSellIn() {
+		return items[position].sellIn;
+	}
 
-    /**
-     * Getter that retrieves quality value.
-     */     
-     public int getItemQuality() {
-          return items[position].quality;
-     }
+	/**
+	 * Getter that retrieves quality value.
+	 */
+	public int getItemQuality() {
+		return items[position].quality;
+	}
 
-    /**
-     * Setter that sets sellIn value.
-     * @param value
-     */     
-     public void setItemSellIn(int value) {
-          items[position].sellIn = value;
-     }
+	/**
+	 * Setter that sets sellIn value.
+	 * 
+	 * @param value
+	 */
+	public void setItemSellIn(int value) {
+		items[position].sellIn = value;
+	}
 
-    /**
-     * Setter that sets quality value.
-     * @param value
-     */
-     public void setItemQuality(int value) {
-          items[position].quality = value;
-     }
+	/**
+	 * Setter that sets quality value.
+	 * 
+	 * @param value
+	 */
+	public void setItemQuality(int value) {
+		items[position].quality = value;
+	}
 
-    /**
-     * Setter that sets the list position.
-     * @param position
-     */
-     public void setPosition(int position) {
-          this.position = position;
-     }
+	/**
+	 * Setter that sets the list position.
+	 * 
+	 * @param position
+	 */
+	public void setPosition(int position) {
+		this.position = position;
+	}
 
-    /**
-     * Function that updates the items quality based on its name.
-     * First adjust the quality and then the sellIn since the later
-     * is a condition to adjust the former.
-     */     
-     @Override
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-             setPosition(i);
-             String name = items[position].name;
-          
-             // Adjust quality
-             switch (name) {
-                 case "Aged Brie":
-                      increaseByItemQuality(1);
-                      break;
-                 case "Sulfuras":
-                      setItemQuality(80);
-                      break;
-                 case "Backstage passes":
-                      updateQualityBackstagePasses();
-                      break;
-                 case "Conjured":
-                      decreaseByItemQuality(2);
-                      break;
-                 default: 
-                      decreaseItemQuality();
-             }
-             
-             // Adjust sellIn
-            if (name.equals("Sulfuras")) {
-                 setItemSellIn(0);
-            } else {
-                 decreaseItemSellIn();
-            }
-        }
-    }
+	/**
+	 * Function that updates the items quality based on its name. First adjust the
+	 * quality and then the sellIn since the later is a condition to adjust the
+	 * former.
+	 */
+	@Override
+	public void updateQuality() {
+		for (int i = 0; i < items.length; i++) {
+			setPosition(i);
+			String name = items[position].name;
 
-    /**
-     * Function that updates a Backstage Passes item.
-     */     
-    private void updateQualityBackstagePasses() {
-         if (items[position].sellIn > 0) {
-              if (items[position].sellIn <= 5) {
-                   increaseByItemQuality(3);
-             } else if (items[position].sellIn <= 10) {
-                  increaseByItemQuality(2);
-             }
-           } else {
-             setItemQuality(0);
-           }
+			// Adjust quality
+			switch (name) {
+			case "Aged Brie":
+				increaseByItemQuality(1);
+				break;
+			case "Sulfuras":
+				setItemQuality(80);
+				break;
+			case "Backstage passes":
+				updateQualityBackstagePasses();
+				break;
+			case "Conjured":
+				decreaseByItemQuality(2);
+				break;
+			default:
+				decreaseItemQuality();
+			}
 
-           decreaseItemSellIn();
-    }
+			// Adjust sellIn
+			if (name.equals("Sulfuras")) {
+				setItemSellIn(0);
+			} else {
+				decreaseItemSellIn();
+			}
+		}
+	}
 
-    /**
-     * Function that decreases the quality of an item by one except
-     * when expired when it will be by two.
-     */         
-    private void decreaseItemQuality() {
-         // 
-         if (items[position].sellIn <= 0) {
-              decreaseByItemQuality(2);              
-         } else {
-              decreaseByItemQuality(1);
-         }
-    }  
+	/**
+	 * Function that updates a Backstage Passes item.
+	 */
+	private void updateQualityBackstagePasses() {
+		if (items[position].sellIn > 0) {
+			if (items[position].sellIn <= 5) {
+				increaseByItemQuality(3);
+			} else if (items[position].sellIn <= 10) {
+				increaseByItemQuality(2);
+			}
+		} else {
+			setItemQuality(0);
+		}
 
-    /**
-     * Function that decreases the quality of an item based on a value
-     * but without becoming negative.
-     * @param value
-     */
-    private void decreaseByItemQuality(int value) {
-         if ((items[position].quality - value) >= 0) {
-              items[position].quality = items[position].quality - value;
-         }
-    }
+		decreaseItemSellIn();
+	}
 
-    /**
-     * Function that increases the quality of an item based on a value
-     * up to MAXQUALITY.
-     * @param value
-     */
-    private void increaseByItemQuality(int value) {
-         if ((items[position].quality + value) <= MAXQUALITY) {
-            items[position].quality = items[position].quality + value;
-        }
-    }  
+	/**
+	 * Function that decreases the quality of an item by one except when expired
+	 * when it will be by two.
+	 */
+	private void decreaseItemQuality() {
+		if (items[position].sellIn <= 0) {
+			decreaseByItemQuality(2);
+		} else {
+			decreaseByItemQuality(1);
+		}
+	}
 
-    /**
-     * Function that decreases the expiration of an item by one.
-     */
-    private void decreaseItemSellIn() {
-         items[position].sellIn--;
-    } 
+	/**
+	 * Function that decreases the quality of an item based on a value but without
+	 * becoming negative.
+	 * 
+	 * @param value
+	 */
+	private void decreaseByItemQuality(int value) {
+		if ((items[position].quality - value) >= 0) {
+			items[position].quality = items[position].quality - value;
+		}
+	}
+
+	/**
+	 * Function that increases the quality of an item based on a value up to
+	 * MAXQUALITY.
+	 * 
+	 * @param value
+	 */
+	private void increaseByItemQuality(int value) {
+		if ((items[position].quality + value) <= MAXQUALITY) {
+			items[position].quality = items[position].quality + value;
+		}
+	}
+
+	/**
+	 * Function that decreases the expiration of an item by one.
+	 */
+	private void decreaseItemSellIn() {
+		items[position].sellIn--;
+	}
 
 }
--- src/test/java/com/gildedrose/GlidedRoseImprovedTest.java
+++ src/test/java/com/gildedrose/GlidedRoseImprovedTest.java
@@ -5,157 +5,158 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 /**
-* Unit tests for GlidedRoseImproved class.
-*
-* @author  diavbolo
-* @version 1.0
-*/
+ * Unit tests for GlidedRoseImproved class.
+ *
+ * @author diavbolo
+ * @version 1.0
+ */
 class GlidedRoseImprovedTest {
 
-    /**
-     * Decrease the sellIn of a normal item
-     */
-    @Test
-    void decreaseSellIn() {
-        Item[] items = new Item[] { new Item("Normal", 11, 10) };
-        GlidedRoseImproved app = new GlidedRoseImproved(items);
-        app.updateQuality();
-        assertEquals(10, app.items[0].sellIn);
-    }
+	/**
+	 * Decrease the sellIn of a normal item
+	 */
+	@Test
+	void decreaseSellIn() {
+		Item[] items = new Item[] { new Item("Normal", 11, 10) };
+		GlidedRoseImproved app = new GlidedRoseImproved(items);
+		app.updateQuality();
+		assertEquals(10, app.items[0].sellIn);
+	}
 
-    /**
-     * Once the sell by date has passed, Quality degrades twice as fast.
-     */
-    @Test
-    void expiredSellIn() {
-        Item[] items = new Item[] { new Item("Normal", 0, 10) };
-        GlidedRoseImproved app = new GlidedRoseImproved(items);
-        app.updateQuality();
-        assertEquals(8, app.getItemQuality());
-    }   
+	/**
+	 * Once the sell by date has passed, Quality degrades twice as fast.
+	 */
+	@Test
+	void expiredSellIn() {
+		Item[] items = new Item[] { new Item("Normal", 0, 10) };
+		GlidedRoseImproved app = new GlidedRoseImproved(items);
+		app.updateQuality();
+		assertEquals(8, app.getItemQuality());
+	}
 
-    /**
-     * The Quality of an item is never negative.
-     */
-    @Test
-    void nonNegativeQuality() {
-        Item[] items = new Item[] { new Item("Normal", 10, 0) };
-        GlidedRoseImproved app = new GlidedRoseImproved(items);
-        app.updateQuality();
-        assertEquals(0, app.getItemQuality());
-    }
+	/**
+	 * The Quality of an item is never negative.
+	 */
+	@Test
+	void nonNegativeQuality() {
+		Item[] items = new Item[] { new Item("Normal", 10, 0) };
+		GlidedRoseImproved app = new GlidedRoseImproved(items);
+		app.updateQuality();
+		assertEquals(0, app.getItemQuality());
+	}
 
-    /**
-     * Decrease the quality of a normal item
-     */
-    @Test
-    void decreaseQuality() {
-        Item[] items = new Item[] { new Item("Normal", 10, 10) };
-        GlidedRoseImproved app = new GlidedRoseImproved(items);
-        app.updateQuality();
-        assertEquals(9, app.getItemQuality());
-    }
+	/**
+	 * Decrease the quality of a normal item
+	 */
+	@Test
+	void decreaseQuality() {
+		Item[] items = new Item[] { new Item("Normal", 10, 10) };
+		GlidedRoseImproved app = new GlidedRoseImproved(items);
+		app.updateQuality();
+		assertEquals(9, app.getItemQuality());
+	}
 
-    /**
-     * The Quality of an item is never more than 50.
-     */
-    @Test
-    void maxQuality() {
-        Item[] items = new Item[] { new Item("Aged Brie", 10, 50) };
-        GlidedRoseImproved app = new GlidedRoseImproved(items);
-        app.updateQuality();
-        assertEquals(50, app.getItemQuality());
-    }    
+	/**
+	 * The Quality of an item is never more than 50.
+	 */
+	@Test
+	void maxQuality() {
+		Item[] items = new Item[] { new Item("Aged Brie", 10, 50) };
+		GlidedRoseImproved app = new GlidedRoseImproved(items);
+		app.updateQuality();
+		assertEquals(50, app.getItemQuality());
+	}
 
-    /**
-     * "Aged Brie" actually increases in Quality the older it gets.
-     */
-    @Test
-    void expiredSellInAgedBrie() {
-        Item[] items = new Item[] { new Item("Aged Brie", -1, 40) };
-        GlidedRoseImproved app = new GlidedRoseImproved(items);
-        app.updateQuality();
-        assertEquals(41, app.getItemQuality());
-    }
+	/**
+	 * "Aged Brie" actually increases in Quality the older it gets.
+	 */
+	@Test
+	void expiredSellInAgedBrie() {
+		Item[] items = new Item[] { new Item("Aged Brie", -1, 40) };
+		GlidedRoseImproved app = new GlidedRoseImproved(items);
+		app.updateQuality();
+		assertEquals(41, app.getItemQuality());
+	}
 
-    /**
-     * "Sulfuras", being a legendary item, never has to be sold or decreases in Quality.
-     */
-    @Test
-    void qualityItemSulfuras() {
-        Item[] items = new Item[] { new Item("Sulfuras", 10, 30) };
-        GlidedRoseImproved app = new GlidedRoseImproved(items);
-        app.updateQuality();
-        assertEquals(80, app.getItemQuality());
-        assertEquals(0, app.getItemSellIn());
-    }
+	/**
+	 * "Sulfuras", being a legendary item, never has to be sold or decreases in
+	 * Quality.
+	 */
+	@Test
+	void qualityItemSulfuras() {
+		Item[] items = new Item[] { new Item("Sulfuras", 10, 30) };
+		GlidedRoseImproved app = new GlidedRoseImproved(items);
+		app.updateQuality();
+		assertEquals(80, app.getItemQuality());
+		assertEquals(0, app.getItemSellIn());
+	}
 
-    /**
-     * "Backstage passes" quality increases by 2 when there are 10 days or less
-     */
-    @Test
-    void tenDaysSellInbaskstagePasses() {
-        Item[] items = new Item[] { new Item("Backstage passes", 10, 6) };
-        GlidedRoseImproved app = new GlidedRoseImproved(items);
-        app.updateQuality();
-        assertEquals(8, app.getItemQuality());
-    }
+	/**
+	 * "Backstage passes" quality increases by 2 when there are 10 days or less
+	 */
+	@Test
+	void tenDaysSellInbaskstagePasses() {
+		Item[] items = new Item[] { new Item("Backstage passes", 10, 6) };
+		GlidedRoseImproved app = new GlidedRoseImproved(items);
+		app.updateQuality();
+		assertEquals(8, app.getItemQuality());
+	}
 
-    /**
-     * "Backstage passes" quality increases by 3 when there are 5 days or less
-     */
-    @Test
-    void fiveDaysSellInbaskstagePasses() {
-        Item[] items = new Item[] { new Item("Backstage passes", 5, 5) };
-        GlidedRoseImproved app = new GlidedRoseImproved(items);
-        app.updateQuality();
-        assertEquals(8, app.getItemQuality());
-    }
+	/**
+	 * "Backstage passes" quality increases by 3 when there are 5 days or less
+	 */
+	@Test
+	void fiveDaysSellInbaskstagePasses() {
+		Item[] items = new Item[] { new Item("Backstage passes", 5, 5) };
+		GlidedRoseImproved app = new GlidedRoseImproved(items);
+		app.updateQuality();
+		assertEquals(8, app.getItemQuality());
+	}
 
-    /**
-     * "Backstage passes" quality drops to 0 after the concert.
-     */
-    @Test
-    void expiredSellInbaskstagePasses() {
-        Item[] items = new Item[] { new Item("Backstage passes", 0, 5) };
-        GlidedRoseImproved app = new GlidedRoseImproved(items);
-        app.updateQuality();
-        assertEquals(0, app.getItemQuality());
-    }
+	/**
+	 * "Backstage passes" quality drops to 0 after the concert.
+	 */
+	@Test
+	void expiredSellInbaskstagePasses() {
+		Item[] items = new Item[] { new Item("Backstage passes", 0, 5) };
+		GlidedRoseImproved app = new GlidedRoseImproved(items);
+		app.updateQuality();
+		assertEquals(0, app.getItemQuality());
+	}
 
-    /**
-     * "Conjured" items degrade in Quality twice as fast as normal items.
-     */
-    @Test
-    void decreaseQualityConjured() {
-        Item[] items = new Item[] { new Item("Conjured", 5, 10) };
-        GlidedRoseImproved app = new GlidedRoseImproved(items);
-        app.updateQuality();
-        assertEquals(8, app.getItemQuality());
-    }
+	/**
+	 * "Conjured" items degrade in Quality twice as fast as normal items.
+	 */
+	@Test
+	void decreaseQualityConjured() {
+		Item[] items = new Item[] { new Item("Conjured", 5, 10) };
+		GlidedRoseImproved app = new GlidedRoseImproved(items);
+		app.updateQuality();
+		assertEquals(8, app.getItemQuality());
+	}
 
-    /**
-     * At the end of each day our system lowers sellIn value
-     */
-    @Test
-    void testGetItemSellIn() {
-        Item[] items = new Item[] { new Item("Normal", 5, 10) };
-        GlidedRoseImproved app = new GlidedRoseImproved(items);
-        app.updateQuality();
-        assertEquals(4, app.getItemSellIn());
-    }
+	/**
+	 * At the end of each day our system lowers sellIn value
+	 */
+	@Test
+	void testGetItemSellIn() {
+		Item[] items = new Item[] { new Item("Normal", 5, 10) };
+		GlidedRoseImproved app = new GlidedRoseImproved(items);
+		app.updateQuality();
+		assertEquals(4, app.getItemSellIn());
+	}
 
-    /**
-     * Test a list of items
-     */
-    @Test
-    void testUpdateQuality() {
-        Item[] items = new Item[] { new Item("Conjured", 5, 10), new Item("Conjured", 4, 9) };
-        GlidedRoseImproved app = new GlidedRoseImproved(items);
-        app.updateQuality();
-        assertEquals(7, app.getItemQuality());
-        app.setPosition(0);
-        assertEquals(8, app.getItemQuality());
-    }
-    
+	/**
+	 * Test a list of items
+	 */
+	@Test
+	void testUpdateQuality() {
+		Item[] items = new Item[] { new Item("Conjured", 5, 10), new Item("Conjured", 4, 9) };
+		GlidedRoseImproved app = new GlidedRoseImproved(items);
+		app.updateQuality();
+		assertEquals(7, app.getItemQuality());
+		app.setPosition(0);
+		assertEquals(8, app.getItemQuality());
+	}
+
 }
